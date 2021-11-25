9e7bdb86f948320e1d7142195e2bd7d181dff139
clarice42 <claramaia820@gmail.com>
2021-11-25 13:45:43 -0300

    ✨ Add user authentication

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::BooksController < ApplicationController
+	before_action :authorized
 
     # Listar todos os livros
 	def index
-		books = Book.order('created_at DESC');
-		render json: {status: 'SUCCESS', message:'Livros carregados', data:books}, status: :ok
+		@books = Book.all
+    	render json: @books
 	end
         
 end
